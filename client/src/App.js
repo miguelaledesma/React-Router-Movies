6bde57afb648c2737fa5783244ade79e40f61c2d
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-02-21 13:33:42 -0800

    finished routing for movie list and for clicking now working on home button

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from "react-router-dom"; 
 import SavedList from './Movies/SavedList';
+//import movielist to use in the route in the return 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +17,10 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+
+          setMovieList(response.data)
+          console.log(response)
+          // console.log(response)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +37,13 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route exact path = "/">
+        <MovieList movies = {movieList} /> 
+         </Route>
+
+       <Route path = "/movies/:id">
+         <Movie /> 
+         </Route>  
     </div>
   );
 }
